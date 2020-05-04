296ca51650485363b923f352e3be116ac865e6ba
Pedro Marques <52288792+Pedr0735@users.noreply.github.com>
2020-05-04 11:35:52 -0300

    Delete database.js

--- database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-
-const connection = new Sequelize('guiaperguntas', 'pedro0735', 'p5kseasus', {
-    host: 'mysql669.umbler.com',
-    port: '41890',
-    dialect: 'mysql'
-})
-
-module.exports = connection;
